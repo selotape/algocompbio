e020f2b84cfc67ccf3e2982963ef44ea79ed2a4b
selotape <RonVisbord@Gmail.com>
2015-04-24 23:01:32 +0300

    improved printing (to csv format)

---
+++ StringMatching/SWTester.py
@@ -0,0 +1,10 @@
+
+from StringMatching.SmithWaterman import printScoreMatrix
+
+
+
+
+
+
+
+
--- StringMatching/SmithWaterman.py
+++ StringMatching/SmithWaterman.py
@@ -2,22 +2,11 @@ __author__ = 'ronvis'
 
 from StringMatching.NWHelper import generateScoreFunction
 
-
 def initMatrix(S, T, sigma):
     # create a zero matrix
     matrix = [[0 for x in range(len(S)+1)] for x in range(len(T)+1)]
 
     gapScore = sigma('a', '_')
-
-    # fill first line & column
-    for i in range(len(S)+1):
-        matrix[0][i] = i*gapScore
-
-    for j in range(len(T)+1):
-        matrix[j][0] = j*gapScore
-
-
-    del i, j
 
     path = ''
     # start hardcore calculation
@@ -27,7 +16,7 @@ def initMatrix(S, T, sigma):
             align = matrix[j-1][i-1] + sigma(S[i-1], T[j-1])
             SGap = matrix[j-1][i] + gapScore
             TGap = matrix[j][i-1] + gapScore
-            pick = max(align, SGap, TGap)
+            pick = max(align, SGap, TGap, 0)
             matrix[j][i] = pick
 
     return matrix
@@ -36,26 +25,17 @@ def initMatrix(S, T, sigma):
 def printScoreMatrix(S, T, sigma):
     matrix = initMatrix(S, T, sigma)
 
-    printCsv(S, T, matrix)
-
-
-
-# print matrix with headers (in csv format)
-def printCsv(S, T, matrix):
-
-    printableS = ',' + ','.join(S)
+    printableS = '       ' + '   '.join(S)
     printableT = ' ' + T
-
     print printableS
-
 
     i = 0
     for row in matrix:
-        print printableT[i] + ',' + ','.join(str(c) for c in row)
+        print printableT[i] + ' ' + str(row)
         i=i+1
 
 
-def test():
+if __name__ == "__main__":
     # S = randomDna(length=5)
     # T = randomDna(length=10)
     S = 'GCATCGATTCCGAGC'
@@ -63,8 +43,3 @@ def test():
     sigma = generateScoreFunction(2, -2, -3)
 
     printScoreMatrix(S, T, sigma)
-
-test()
-
-
-
